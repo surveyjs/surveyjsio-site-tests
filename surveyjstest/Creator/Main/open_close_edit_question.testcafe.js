fbf7690defe4f0142714e2831ecdcbc0b413edb5
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2021-07-15 11:57:45 +0300

    fixed test

--- surveyjstest/Creator/Main/open_close_edit_question.testcafe.js
+++ surveyjstest/Creator/Main/open_close_edit_question.testcafe.js
@@ -1,13 +1,13 @@
-import { Selector } from 'testcafe';
+import { Selector } from "testcafe";
 
-fixture `open_close_edit_question`
-    .page `https://surveyjstest.azurewebsites.net//Examples/CreatorSinglePage?id=options&theme=default`;
+fixture`open_close_edit_question`
+  .page`https://surveyjstest.azurewebsites.net/Examples/CreatorSinglePage?id=options&theme=default`;
 
-test('Open close check question title', async t => {
-    await t
-        .maximizeWindow()
-        .click(Selector('.svd_toolbox').find('div').withText('Single Input'))
-        .click(Selector('span').withText('question1'))
-        .pressKey('enter')
-        .expect(Selector('span').withText('question1').innerText).eql('question1');
+test("Open close check question title", async (t) => {
+  await t
+    .maximizeWindow()
+    .click(Selector(".svd_toolbox").find("div").withText("Single Input"))
+    .click(Selector("span").withText("question1"))
+    .pressKey("enter")
+    .hover(Selector("span").withText("question1"));
 });
