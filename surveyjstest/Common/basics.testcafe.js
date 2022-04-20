b49db21d3a6190dfda4b329508d15440e892f988
OlgaLarina <olga.larina.dev@gmail.com>
2022-04-20 17:57:59 +0400

    fix test

--- surveyjstest/Common/basics.testcafe.js
+++ surveyjstest/Common/basics.testcafe.js
@@ -1,35 +1,34 @@
-import { Selector, ClientFunction } from "testcafe";
+import { Selector } from "testcafe";
 
 fixture`Basics`.page`http://surveyjstest.azurewebsites.net`;
 
 test("Menu Examples test", async (t) => {
   await t.maximizeWindow();
 
-  const ProductsMenuLibrary = Selector("a").withText("LIBRARY");
-  const ProductsMenuLibraryExamples = Selector("a")
-    .withText("Examples")
-    .filterVisible();
-  const LibraryExamplesMenuItem = Selector(
-    ".product-menu__item-text--active"
-  ).withText("Examples");
+  const menuItems = Selector(".products-menu__item .popup-menu__item.popup-menu__item--active");
+  const visiblePopup = Selector(".popup-menu--expanded .popup-menu-content").filterVisible();
+  const visiblePopupItems = Selector(".popup-menu--expanded .popup-menu-content .popup-menu__item").filterVisible();
 
-  await t.hover(ProductsMenuLibrary, { speed: 0.5, offsetX: 30, offsetY: 30 });
-  await t.hover(ProductsMenuLibraryExamples, {
-    speed: 0.5,
-    offsetX: 30,
-    offsetY: 30,
-  });
-  await t.click(ProductsMenuLibraryExamples, {
-    speed: 0.5,
-    offsetX: 30,
-    offsetY: 30,
-  });
-  await t.hover(LibraryExamplesMenuItem, { speed: 0.5 });
+  await t
+  .expect(menuItems.count).eql(3)
+  .expect(visiblePopup.count).eql(0)
+  .expect(visiblePopupItems.count).eql(0)
+
+  .hover(menuItems.nth(0), { speed: 0.5, offsetX: 30, offsetY: 30 })
+  .expect(visiblePopup.count).eql(1)
+  .expect(visiblePopupItems.count).eql(5)
+
+  .hover(menuItems.nth(1), { speed: 0.5, offsetX: 30, offsetY: 30 })
+  .expect(visiblePopup.count).eql(1)
+  .expect(visiblePopupItems.count).eql(6)
+
+  .hover(menuItems.nth(2), { speed: 0.5, offsetX: 30, offsetY: 30 })
+  .expect(visiblePopup.count).eql(1)
+  .expect(visiblePopupItems.count).eql(7);
 });
 
 test("Logo Test", async (t) => {
   await t
     .maximizeWindow()
-    .expect(Selector("h3").withText("Survey Creator").innerText)
-    .eql("Survey Creator");
+    .expect(Selector("h3").withText("Survey Creation").exists).ok();
 });
