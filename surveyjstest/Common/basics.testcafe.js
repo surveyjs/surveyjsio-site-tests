73d7c3b47d7bed41afb8f91402c0e187a7195381
dmitrykurmanov <kurmanov.work@gmail.com>
2022-04-27 12:51:12 +0300

    fixed func test

--- surveyjstest/Common/basics.testcafe.js
+++ surveyjstest/Common/basics.testcafe.js
@@ -2,29 +2,29 @@ import { Selector } from "testcafe";
 
 fixture`Basics`.page`http://surveyjstest.azurewebsites.net`;
 
-test("Menu Examples test", async (t) => {
+test.only("Menu Examples test", async (t) => {
   await t.maximizeWindow();
 
-  const menuItems = Selector(".products-menu__item .popup-menu__item.popup-menu__item--active");
-  const visiblePopup = Selector(".popup-menu--expanded .popup-menu-content").filterVisible();
-  const visiblePopupItems = Selector(".popup-menu--expanded .popup-menu-content .popup-menu__item").filterVisible();
+  const menuItems = Selector(".page-menu-bar:not(.page-menu-bar--sticky) .popup-menu__item.popup-menu__item--active");
+  const visiblePopup = Selector(".page-menu-bar:not(.page-menu-bar--sticky) .popup-menu--expanded .popup-menu-content").filterVisible();
+  const visiblePopupItems = Selector(".page-menu-bar:not(.page-menu-bar--sticky) .popup-menu--expanded .popup-menu-content .popup-menu__item").filterVisible();
 
   await t
-  .expect(menuItems.count).eql(3)
-  .expect(visiblePopup.count).eql(0)
-  .expect(visiblePopupItems.count).eql(0)
+    .expect(menuItems.count).eql(3)
+    .expect(visiblePopup.count).eql(0)
+    .expect(visiblePopupItems.count).eql(0)
 
-  .hover(menuItems.nth(0), { speed: 0.5, offsetX: 30, offsetY: 30 })
-  .expect(visiblePopup.count).eql(1)
-  .expect(visiblePopupItems.count).eql(5)
+    .hover(menuItems.nth(0), { speed: 0.5, offsetX: 30, offsetY: 30 })
+    .expect(visiblePopup.count).eql(1)
+    .expect(visiblePopupItems.count).eql(5)
 
-  .hover(menuItems.nth(1), { speed: 0.5, offsetX: 30, offsetY: 30 })
-  .expect(visiblePopup.count).eql(1)
-  .expect(visiblePopupItems.count).eql(6)
+    .hover(menuItems.nth(1), { speed: 0.5, offsetX: 30, offsetY: 30 })
+    .expect(visiblePopup.count).eql(1)
+    .expect(visiblePopupItems.count).eql(6)
 
-  .hover(menuItems.nth(2), { speed: 0.5, offsetX: 30, offsetY: 30 })
-  .expect(visiblePopup.count).eql(1)
-  .expect(visiblePopupItems.count).eql(7);
+    .hover(menuItems.nth(2), { speed: 0.5, offsetX: 30, offsetY: 30 })
+    .expect(visiblePopup.count).eql(1)
+    .expect(visiblePopupItems.count).eql(7);
 });
 
 test("Logo Test", async (t) => {
