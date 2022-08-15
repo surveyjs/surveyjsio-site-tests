54944699cc69a101162250fc2036e50b32807c80
dmitrykurmanov <kurmanov.work@gmail.com>
2022-08-15 14:49:14 +0300

    added tests for V2 (testimonals)

--- V2/tests/Index-Page.testcafe.js
+++ V2/tests/Index-Page.testcafe.js
@@ -7,6 +7,14 @@ for (const screenName in screens) {
   const screen = screens[screenName];
   const height = 2000;
 
+  test(`Testimonials-Section--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
+
+    const TopBar = Selector(".v2-class---index-page__testimonials-section").filterVisible();
+    await checkElementScreenshot(`Index-Page-Testimonials-Section--${screenName}.png`, TopBar, t);
+  });
+
   test(`Ending-Section--${screenName}`, async (t) => {
     await t.resizeWindow(screen.width, height);
     await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
