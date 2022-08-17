aeb3a0aecdfd6c99d1a2df4e94890f351c544317
dmitrykurmanov <kurmanov.work@gmail.com>
2022-08-17 11:36:02 +0300

    added tests for V2 (sections)

--- V2/tests/Index-Page.testcafe.js
+++ V2/tests/Index-Page.testcafe.js
@@ -7,6 +7,46 @@ for (const screenName in screens) {
   const screen = screens[screenName];
   const height = 2000;
 
+  test.only(`Title-Section--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
+
+    const TopBar = Selector(".v2-class---title-section").filterVisible();
+    await checkElementScreenshot(`Index-Page-Title-Section--${screenName}.png`, TopBar, t);
+  });
+
+  test.only(`Title-Image-Section--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
+
+    const TopBar = Selector(".v2-class---title-image-section").filterVisible();
+    await checkElementScreenshot(`Index-Page-Title-Image-Section--${screenName}.png`, TopBar, t);
+  });
+
+  test.only(`Customers-Section--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
+
+    const TopBar = Selector(".v2-class---customers-section").filterVisible();
+    await checkElementScreenshot(`Index-Page-Customers-Section--${screenName}.png`, TopBar, t);
+  });
+
+  test.only(`Features-Section--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
+
+    const TopBar = Selector(".v2-class---features-section").filterVisible();
+    await checkElementScreenshot(`Index-Page-Features-Section--${screenName}.png`, TopBar, t);
+  });
+
+  test.only(`Gallery-Section--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
+
+    const TopBar = Selector(".v2-class---gallery-section").filterVisible();
+    await checkElementScreenshot(`Index-Page-Gallery-Section--${screenName}.png`, TopBar, t);
+  });
+
   test(`Products-Section--${screenName}`, async (t) => {
     await t.resizeWindow(screen.width, height);
     await t.click(Selector(".surveyjs-cookie-info__span")); // close cookie msg
