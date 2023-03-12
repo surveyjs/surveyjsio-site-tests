e244d28727d9585773c193eead1357f4e2bd7966
Aleksey Novikov <novikov@abrisplatform.com>
2023-03-12 21:20:44 +0300

    form-library and survey-creator pages tests

---
+++ V2/tests/Form-Library-Page.testcafe copy.js
@@ -0,0 +1,25 @@
+import { ClientFunction, Selector } from "testcafe";
+import { url, checkElementScreenshot, screens, explicitErrorHandler, disableSmoothScroll } from "../helper";
+
+const route = "/form-library";
+
+fixture`FormLibraryPage`.page`${url}${route}`.beforeEach(async t => {
+    await explicitErrorHandler();
+    await disableSmoothScroll();
+    
+    const cookiePopupAccept = Selector(".v2-class---cookies-popup__button-container a");
+    if(await cookiePopupAccept.exists) {
+      await t.click(cookiePopupAccept); // close cookie msg
+    } 
+});
+
+for (const screenName in screens) {
+  const screen = screens[screenName];
+  const height = 10000;
+  test(`Form-Library-Page--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    await ClientFunction(()=>{ document.getElementById("configure-web-forms").style.display = "none"})();
+    const Page = Selector(".v2-class---form-library-page").filterVisible();
+    await checkElementScreenshot(`Form-Library-Page--${screenName}.png`, Page, t);
+  });
+}
---
+++ V2/tests/Survey-Creator-Page.testcafe.js
@@ -0,0 +1,24 @@
+import { ClientFunction, Selector } from "testcafe";
+import { url, checkElementScreenshot, screens, explicitErrorHandler, disableSmoothScroll } from "../helper";
+
+const route = "/survey-creator";
+
+fixture`SurveyCreatorPage`.page`${url}${route}`.beforeEach(async t => {
+    await explicitErrorHandler();
+    await disableSmoothScroll();
+    
+    const cookiePopupAccept = Selector(".v2-class---cookies-popup__button-container a");
+    if(await cookiePopupAccept.exists) {
+      await t.click(cookiePopupAccept); // close cookie msg
+    } 
+});
+
+for (const screenName in screens) {
+  const screen = screens[screenName];
+  const height = 10000;
+  test.only(`Survey-Creator-Page--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
+    const Page = Selector(".v2-class---survey-creator-page").filterVisible();
+    await checkElementScreenshot(`Survey-Creator-Page--${screenName}.png`, Page, t);
+  });
+}
