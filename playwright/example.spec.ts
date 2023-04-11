e92868c9db51626e4e2e212138547f2fd3d25c62
dmitrykurmanov <kurmanov.work@gmail.com>
2023-04-11 19:50:38 +0400

    work for the https://github.com/surveyjs/service/issues/1032

---
+++ playwright/example.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+const url = "https://surveyjstest.azurewebsites.net/";
+
+test('SurveyJS Site Index Page', async ({ page }) => {
+  const height = 10000;
+  const width = 1920;
+  await page.goto(url);
+  page.setViewportSize({ width, height });
+
+  await expect(page).toHaveScreenshot();
+});
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
