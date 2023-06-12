f8bc3be3563a83b7102dfbd30061f059eb101bd9
dmitrykurmanov <kurmanov.work@gmail.com>
2023-06-12 18:46:54 +0400

    fixed stay-updated test (blinking)

--- V2/tests/Stay-Updated-Blog-Page.testcafe.js
+++ V2/tests/Stay-Updated-Blog-Page.testcafe.js
@@ -1,11 +1,19 @@
-import { Selector } from "testcafe";
+import { Selector, ClientFunction } from "testcafe";
 import { url, checkElementScreenshot, screens, explicitErrorHandler, disableSmoothScroll } from "../helper";
 
 const route = "/stay-updated/blog";
 
+const removeNewItemsExcept5 = ClientFunction(() => {
+  const items = document.querySelectorAll(".v2-class---stay-updated-page__articles-list-item");
+  items.forEach((item, index)=>{
+    if (index < items.length - 5 ) item.remove();
+  });
+});
+
 fixture`StayUpdatedBlog-Page`.page`${url}${route}`.beforeEach(async t => {
     await explicitErrorHandler();
     await disableSmoothScroll();
+    await removeNewItemsExcept5();
     
     const cookiePopupAccept = Selector(".v2-class---popup__button-container a");
     if(await cookiePopupAccept.exists) {
@@ -16,9 +24,9 @@ fixture`StayUpdatedBlog-Page`.page`${url}${route}`.beforeEach(async t => {
 for (const screenName in screens) {
   const screen = screens[screenName];
   const height = 10000;
-  test.only(`StayUpdatedBlog-Page--${screenName}`, async (t) => {
-    await t.resizeWindow(screen.width, height);
 
+  test(`StayUpdatedBlog-Page--${screenName}`, async (t) => {
+    await t.resizeWindow(screen.width, height);
     const Page = Selector(".v2-class---stay-updated-page").filterVisible();
     await checkElementScreenshot(`StayUpdatedBlog-Page--${screenName}.png`, Page, t);
   });
