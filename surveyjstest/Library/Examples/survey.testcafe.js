5e14be5ba9861fd6806de4cf7439772d38934917
dmitrykurmanov <kurmanov.work@gmail.com>
2023-06-28 18:26:10 +0400

    fixed "title and logo" test

--- surveyjstest/Library/Examples/survey.testcafe.js
+++ surveyjstest/Library/Examples/survey.testcafe.js
@@ -8,19 +8,19 @@ fixture`survey`
         await acceptCookie(t);
     });
 
-test('title_logo', async t => {
+test.only('title_logo', async t => {
     await t
         .maximizeWindow()
         .click(getSideBarGroupItem('Survey'))
-        .click(getSideBarItem('Title and Logo'))
+        .click(getSideBarItem('Branded Survey Header'))
         .expect(Selector('.sd-logo__image').getAttribute('src')).ok('Logo is visible')
-        .expect(Selector('#surveyElement').find('span').withText('Survey Title&Logo demo').visible).ok('Title is visible')
-        .expect(Selector('#surveyElement').find('span').withText('Please take look at the survey title and logo. Tes').visible).ok('Description is visible')
+        .expect(Selector('#surveyElement').find('span').withText('NPS Survey Question').visible).ok('Title is visible')
+        .expect(Selector('#surveyElement').find('span').withText('NPS (net promoter score) is a metric used to evaluate customer loyalty and business growth').visible).ok('Description is visible')
         .expect(Selector('.sd-logo.sv-logo--left').exists).eql(true)
         .expect(Selector('.sd-logo.sv-logo--right').exists).eql(false)
         .click("#tool-settings")
-        .click('#selColCount')
-        .click(Selector('#selColCount').find('option').withText('right'))
+        .click('#logoPosition')
+        .click(Selector('#logoPosition').find('option').withText('right'))
         .expect(Selector('.sd-logo.sv-logo--left').exists).eql(false)
         .expect(Selector('.sd-logo.sv-logo--right').exists).eql(true);
 });
