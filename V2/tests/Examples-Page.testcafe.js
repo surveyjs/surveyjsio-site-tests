80030537e2a9714532b1dc71e8217d1975f1c6ed
dmitrykurmanov <kurmanov.work@gmail.com>
2023-08-01 18:35:01 +0400

    work for the https://github.com/surveyjs/service/issues/1295 (test)

--- V2/tests/Examples-Page.testcafe.js
+++ V2/tests/Examples-Page.testcafe.js
@@ -4,20 +4,20 @@ import { url, checkElementScreenshot, screens, explicitErrorHandler, disableSmoo
 const route = "/form-library/examples/single-select-radio-button-group/reactjs";
 
 fixture`ExamplesPage`.page`${url}${route}`.beforeEach(async t => {
-    await explicitErrorHandler();
-    await disableSmoothScroll();
-    
-    const cookiePopupAccept = Selector(".v2-class---popup__button-container a");
-    if(await cookiePopupAccept.exists) {
-      await t.click(cookiePopupAccept); // close cookie msg
-    } 
+  await explicitErrorHandler();
+  await disableSmoothScroll();
+
+  const cookiePopupAccept = Selector(".v2-class---popup__button-container a");
+  if (await cookiePopupAccept.exists) {
+    await t.click(cookiePopupAccept); // close cookie msg
+  }
 });
 
 for (const screenName in screens) {
   const screen = screens[screenName];
   const height = 900;
   test(`Examples-Page--${screenName}`, async (t) => {
-      await wrapVisualTest(t, async (t, comparer) => {      
+    await wrapVisualTest(t, async (t, comparer) => {
       await t.resizeWindow(screen.width, height);
 
       const toggleThemes = ClientFunction(() => {
@@ -34,38 +34,38 @@ for (const screenName in screens) {
       await toggleThemes();
 
       await t
-      .click("#tool-theme")
+        .click("#tool-theme")
       await takeElementScreenshot(`Examples-Page-Theme--${screenName}.png`, Page, t, comparer);
       await t
-      .click("#tool-theme");
-      
+        .click("#tool-theme");
+
       await t
-      .click("#tool-settings");
+        .click("#tool-settings");
       await takeElementScreenshot(`Examples-Page-Settings--${screenName}.png`, Page, t, comparer);
       await t
-      .click("#tool-settings");
+        .click("#tool-settings");
 
-      if(screenName == "Mobile" || screenName == "Vertical-Tablet") {
+      if (screenName == "Mobile" || screenName == "Vertical-Tablet") {
         await t
-        .click("#button-sidebar");
+          .click("#button-sidebar");
         await takeElementScreenshot(`Examples-Page-Sidebar--${screenName}.png`, Page, t, comparer);
         await t
-        .click("#button-sidebar");
+          .click("#button-sidebar");
       }
-      if(screenName != "Mobile") {
+      if (screenName != "Mobile") {
         await t
-        .hover(Selector(".v2-class---demo-header__toolbar .v2-class---header-toolbar-item-dropdown").nth(1))
-        .expect(Selector(".v2-class---drop-down-menu-item__link").withText("React").visible).ok()
-        .wait(500);
+          .hover(Selector(".v2-class---demo-header__toolbar .v2-class---header-toolbar-item-dropdown").nth(1))
+          .expect(Selector(".v2-class---drop-down-menu-item__link").withText("React").visible).ok()
+          .wait(500);
 
         await takeElementScreenshot(`Examples-Page-EditIn--${screenName}.png`, Page, t, comparer);
 
         await t
-        .click("#tab-code");
+          .click("#tab-code");
         await takeElementScreenshot(`Examples-Page-Code--${screenName}.png`, Page, t, comparer);
-  
+
         await t
-        .click("#tab-docs");
+          .click("#tab-docs");
         await takeElementScreenshot(`Examples-Page-Docs--${screenName}.png`, Page, t, comparer);
       }
 
@@ -74,3 +74,18 @@ for (const screenName in screens) {
     });
   });
 }
+
+test(`Examples-Page--Result-JSON-PDF`, async (t) => {
+  await wrapVisualTest(t, async (t, comparer) => {
+    const screen = screens["Large-Desktop"]
+    await t.resizeWindow(screen.width, screen.height);
+
+    await t.click("#surveyElement .sd-selectbase__label");
+    await t.click("#surveyElement  input[value='Complete']");
+
+    const DemoResult = Selector("#demo-result").filterVisible();
+    await takeElementScreenshot(`Examples-Page--Result-JSON-PDF.png`, DemoResult, t, comparer);
+
+    await t.resizeWindow(929, 932);
+  });
+});
