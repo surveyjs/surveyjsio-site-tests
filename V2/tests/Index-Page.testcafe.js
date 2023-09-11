b2acd428223169b7512b396c49218392e90e9cf3
Aleksey Novikov <novikov82@gmail.com>
2023-09-11 18:56:32 +0300

    update inex page screenshots

--- V2/tests/Index-Page.testcafe.js
+++ V2/tests/Index-Page.testcafe.js
@@ -1,4 +1,4 @@
-import { Selector } from "testcafe";
+import { Selector, ClientFunction } from "testcafe";
 import { url, checkElementScreenshot, screens, explicitErrorHandler, disableSmoothScroll } from "../helper";
 
 fixture`IndexPage`.page`${url}`.beforeEach(async t => {
@@ -16,6 +16,8 @@ for (const screenName in screens) {
   const height = 10000;
   test(`Index-Page--${screenName}`, async (t) => {
     await t.resizeWindow(screen.width, height);
+
+    await ClientFunction(()=>{ document.querySelector("video").style.visibility = "hidden"; })();
 
     const Page = Selector(".v2-class---index-page").filterVisible();
     await checkElementScreenshot(`Index-Page--${screenName}.png`, Page, t);
