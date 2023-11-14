af6f83fc9518d40ffc2c43031459e4fdb1f0fa9e
dmitrykurmanov <kurmanov.work@gmail.com>
2023-11-14 14:24:18 +0400

    work for the https://github.com/surveyjs/service/issues/1639

---
+++ playwright/licences-expired-notifications.spec.ts
@@ -0,0 +1,96 @@
+import { test, expect } from '@playwright/test';
+
+test("licences expired notifications", async ({ page }) => {
+  const testerEmail = "Sych-Test1@gmail.com";
+  const testerPass = "Sych-Test1@gmail.com";
+  const url = "https://surveyjstest.azurewebsites.net";
+  //const url = "http://localhost:62946/";
+  const apiUrl = "api/ManageUser/getLicensesExpirationPopupType?expiresSoonNotificationDateTime=null&expiredNotificationDateTime=null";
+  const bannerExpiredTitle = "Your renewal subscription has expired.";
+  const bannerExpiresSoonTitle = "Your renewal subscription expires soon.";
+  const topBarIconTwinklingClass = ".v2-class---top-menu-item__icon--twinkling";
+  const topBarAccountClass = ".v2-class---top-menu-item--drop-down-account";
+  const topBarAccountSettingsWarningClass = ".v2-class---drop-down-menu-item--manage-warning";
+
+  let apiRequestCount = 0;
+
+  const mockPopupType = async (popupType)=>{
+    // Mock the api call before navigating
+    await page.route(`*/**/${apiUrl}`, async route => {
+      /*
+        PopupType could be "none", "expired", "expiresSoon", "warningOnly"
+      */
+      apiRequestCount++;
+      const json = { popupType };
+      await route.fulfill({ json });
+    });
+  }
+  const removeExpirationCookie = async ()=>{
+    await page.evaluate(() => {
+      document.cookie = "expirationCookie" + "=; Max-Age=0";
+    });
+    
+  }
+
+  test.setTimeout(480000);
+  await mockPopupType("none");
+
+  // none notifications
+  await page.goto(`${url}/login`);
+  await page.locator('a').filter({ hasText: 'Accept All' }).click(); // hide cookie banner
+  await page.getByPlaceholder('Email').fill(testerEmail);
+  await page.getByPlaceholder('Password').fill(testerPass);
+  await page.locator('label').filter({ hasText: 'I have read, understand and accept the surveyjs.io website Terms of Use and Priv' }).click();
+  await page.locator('.v2-class---signup-page__actions-footer-button-container--login').click();
+  await expect(page.getByText(bannerExpiredTitle)).toBeHidden();
+  await expect(page.getByText(bannerExpiresSoonTitle)).toBeHidden();
+  await expect(page.locator(topBarIconTwinklingClass).first()).toBeHidden();
+  await page.locator(topBarAccountClass).first().click();
+  await expect(await page.locator(topBarAccountSettingsWarningClass).count()).toEqual(0);
+  await expect(apiRequestCount).toEqual(1);
+
+  // expired notifications
+  await removeExpirationCookie();
+  await mockPopupType("expired");
+  await page.goto(`${url}`);
+  await expect(page.getByText(bannerExpiredTitle)).toBeVisible();
+  await expect(page.getByText(bannerExpiresSoonTitle)).toBeHidden();
+  await expect(page.locator(topBarIconTwinklingClass).first()).toBeVisible();
+  await page.locator(topBarAccountClass).first().click();
+  await expect(page.locator(topBarAccountSettingsWarningClass).first()).toBeVisible();
+  await expect(apiRequestCount).toEqual(2);
+
+  // expiresSoon notifications
+  await removeExpirationCookie();
+  await mockPopupType("expiresSoon");
+  await page.goto(`${url}`);
+  await expect(page.getByText(bannerExpiredTitle)).toBeHidden();
+  await expect(page.getByText(bannerExpiresSoonTitle)).toBeVisible();
+  await expect(page.locator(topBarIconTwinklingClass).first()).toBeVisible();
+  await page.locator(topBarAccountClass).first().click();
+  await expect(page.locator(topBarAccountSettingsWarningClass).first()).toBeVisible();
+  await expect(apiRequestCount).toEqual(3);
+
+  // warningOnly notifications
+  await removeExpirationCookie();
+  await mockPopupType("warningOnly");
+  await page.goto(`${url}`);
+  await expect(page.getByText(bannerExpiredTitle)).toBeHidden();
+  await expect(page.getByText(bannerExpiresSoonTitle)).toBeHidden();
+  await expect(page.locator(topBarIconTwinklingClass).first()).toBeHidden();
+  await page.locator(topBarAccountClass).first().click();
+  await expect(page.locator(topBarAccountSettingsWarningClass).first()).toBeVisible();
+  await expect(apiRequestCount).toEqual(4);
+
+  // close banner after visit Account Page
+  await removeExpirationCookie();
+  await mockPopupType("expired");
+  await page.goto(`${url}`);
+  await expect(page.getByText(bannerExpiredTitle)).toBeVisible();
+  await page.goto(`${url}/manage`);
+  await expect(page.getByText(bannerExpiredTitle)).toBeHidden();
+  await page.goto(`${url}`);
+  await expect(page.getByText(bannerExpiredTitle)).toBeHidden();
+  await expect(apiRequestCount).toEqual(5);
+});
+
