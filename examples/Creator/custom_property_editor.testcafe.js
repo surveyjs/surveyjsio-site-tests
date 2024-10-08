8187d01288875daac42495d15fa8abcd6b36352a
LARINA-NB-W10\OlgaLarina <olga.larina.dev@gmail.com>
2024-10-08 15:35:30 +0300

    update test

--- examples/Creator/custom_property_editor.testcafe.js
+++ examples/Creator/custom_property_editor.testcafe.js
@@ -4,10 +4,10 @@ fixture `custom_property_editor`
   .page `https://surveyjstest.azurewebsites.net/survey-creator/examples/s/customize-property-editors/reactjs`;
 
 test('Check shortname property', async t => {
-  const shortnameEditor = Selector('.sv-string-viewer').withText('Shortname').parent('.spg-question').find('input');
+  const shortnameEditor = Selector('.sv-string-viewer').withText('Short name').parent('.spg-question').find('input');
   await t
     .maximizeWindow()
     .click(Selector('.svc-add-new-question-action'))
-    .typeText(shortnameEditor, '123456789')
-    .expect(shortnameEditor.value).eql('12345');
+    .typeText(shortnameEditor, '123456789101112131415')
+    .expect(shortnameEditor.value).eql('123456789101112');
 });
