a9a7947951741ee9c3b49e256d06246779cce5dc
dmitrykurmanov <kurmanov.work@gmail.com>
2025-09-17 15:12:41 +0400

    work for the https://github.com/surveyjs/service/issues/3401

--- examples-playwright/embedded-tabs.spec.ts
+++ examples-playwright/embedded-tabs.spec.ts
@@ -8,12 +8,21 @@ const url = domain + '/create-free-survey';
 test('Survey Embeded & Creator Embeded', async ({ page }) => {
   test.setTimeout(480000);
 
+  const errors:any = [];
+  page.on('pageerror', (error) => {
+    errors.push(error);
+  });
+
   await page.goto(url);
   await page.locator('a').filter({ hasText: 'Accept All' }).click();
   await page.locator('#site-tour').getByText('Later').click();
   await page.setViewportSize({ width: 1920, height: 1080 });
   await page.getByRole('tab', { name: 'Embed Survey' }).locator('span').click();
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(5000);
+  await page.getByText('To add the survey to your').hover();
   await page.getByRole('tab', { name: 'Embed Creator' }).locator('span').click();
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(5000);
+  await page.getByText('To add Survey Creator to your').hover();
+
+  expect(errors).toHaveLength(0);
 });
