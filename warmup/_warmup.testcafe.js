627c5fb374c7805248bdec36616c4a488248f90e
dmitrykurmanov <kurmanov.work@gmail.com>
2025-11-18 14:36:42 +0400

    Move site+site-exampels tests to Playwright #22

--- warmup/_warmup.testcafe.js
+++
@@ -1,9 +0,0 @@
-import { Selector, fixture, test } from 'testcafe';
-
-fixture `warmup`
-    .page `https://surveyjsio-test.azurewebsites.net/Account/Login`;
-
-test('Warmup If Site Is Updating', async t => {
-    const article = Selector('article', { timeout: 1000000 }).exists;
-    await t.expect(article).ok();
-});
